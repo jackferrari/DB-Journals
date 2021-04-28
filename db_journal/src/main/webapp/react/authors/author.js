3e33c2d305efd548ce21c08cf7dc43f2f0f840e7
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-28 17:22:33 -0400

    Update author.js

--- db_journal/src/main/webapp/react/authors/author.js
+++ db_journal/src/main/webapp/react/authors/author.js
@@ -1,5 +1,8 @@
 import AuthorList from "./author-content/author-list";
 import AuthorFormEditor from "./author-content/author-form-editor";
+import ArticlesList from "./author-content/author-articles-list";
+import UsersList from "./author-content/author-users-list";
+
 const {HashRouter, Route} = window.ReactRouterDOM;
 const App = () => {
     return (
@@ -10,6 +13,12 @@ const App = () => {
                 </Route>
                 <Route path="/authors/:id" exact={true}>
                     <AuthorFormEditor/>
+                </Route>
+                <Route path={["/authors", "/"]} exact={true}>
+                    <ArticlesList/>
+                </Route>
+                <Route path={["/authors", "/"]} exact={true}>
+                    <UsersList/>
                 </Route>
             </HashRouter>
         </div>
