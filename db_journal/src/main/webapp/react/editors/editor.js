fcdfa7c954debd54c85c2e6b4457a97549eda3f3
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-28 21:21:19 -0400

    Update editor.js

--- db_journal/src/main/webapp/react/editors/editor.js
+++ db_journal/src/main/webapp/react/editors/editor.js
@@ -1,5 +1,8 @@
 import EditorList from "./editor-content/editor-list";
 import EditorFormEditor from "./editor-content/editor-form-editor";
+import ArticlesList from "./editor-content/editor-articles-list";
+import UsersList from "./editor-content/editor-users-list";
+
 const {HashRouter, Route} = window.ReactRouterDOM;
 const App = () => {
     return (
@@ -10,6 +13,12 @@ const App = () => {
                 </Route>
                 <Route path="/editors/:id" exact={true}>
                     <EditorFormEditor/>
+                </Route>
+                <Route path="/editors/:id/articles" exact={true}>
+                    <ArticlesList/>
+                </Route>
+                <Route path="/editors/:id/users" exact={true}>
+                    <UsersList/>
                 </Route>
             </HashRouter>
         </div>
